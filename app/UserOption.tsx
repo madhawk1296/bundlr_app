66d87a060673a864809eb9e468c1f5752ff7e976
madhawk1296 <silvacristian1296@gmail.com>
2023-07-13 15:44:33 -0600

    fix

--- app/UserOption.tsx
+++ app/UserOption.tsx
@@ -4,11 +4,15 @@ import { useDisconnect } from "wagmi"
 export default function UserOption({ handleCloseMenu, title, link=null, isDisconnect=false }) {
     const { disconnect } = useDisconnect();
 
+    const handleClick = () => {
+        isDisconnect ? disconnect() : handleCloseMenu()
+    }
+
     return isDisconnect ? (
-        <h1 onClick={disconnect} className={`cursor-pointer w-full text-center py-[10px] text-md font-medium text-red-500 bg-white`}>{title}</h1>
+        <h1 onClick={handleClick} className={`cursor-pointer w-full text-center py-[10px] text-md font-medium text-red-500 bg-white`}>{title}</h1>
     ) : (
         <Link href={link} >
-            <h1 onClick={handleCloseMenu} className={`cursor-pointer w-full text-center py-[10px] text-md font-medium text-gray-800 bg-white`}>{title}</h1>
+            <h1 onClick={handleClick} className={`cursor-pointer w-full text-center py-[10px] text-md font-medium text-gray-800 bg-white`}>{title}</h1>
         </Link>
     )
 }
