4ecab92ac201ff91d0a83db19984f4ae2690e33e
madhawk1296 <silvacristian1296@gmail.com>
2023-07-13 16:35:35 -0600

    fix

--- lib/prepareApproveMarketplace.ts
+++ lib/prepareApproveMarketplace.ts
@@ -3,12 +3,12 @@ import approvalABI from "../data/abi/approvalABI";
 
 export default function prepareApproveMarketplace(address) {
     const abi = approvalABI();
-
+    
     const { config } = usePrepareContractWrite({
         address,
         abi,
         functionName: 'setApprovalForAll',
-        args: [process.env.NEXT_PUBLIC_BUNDLR_ADDRESS, true],
+        args: [process.env.NEXT_PUBLIC_BUNDLR_ADDRESS, true] as never,
     })
 
     return config;
